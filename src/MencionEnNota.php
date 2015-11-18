fe72ece0013c38c5d95abb63b5348875547b28fd
Emir Herrera <emir.herrera@imco.org.mx>
2015-11-17 21:18:26 -0600

    Puto laravel de mierda que no dice dónde ocurre la exepción

--- src/MencionEnNota.php
+++ src/MencionEnNota.php
@@ -9,7 +9,7 @@ class MencionEnNota extends Model
     protected $connection = 'catalogoNoms';
     protected $primaryKey = 'id_mencion_en_nota';
     protected $table = 'menciones_en_notas';
-    protected $fillable = ["clave","cod_nota", "ubicacion"];
+    protected $fillable = ["mencion","cod_nota", "ubicacion"];
     
 
     public function nota(){
