b4a9f24dc5f71ae411f3c2081bef112448ed68ad
Emir Herrera <emir.herrera@imco.org.mx>
2015-11-17 20:31:48 -0600

    Mencion column

--- src/database/seeds/MencionEnNotaSeeder.php
+++ src/database/seeds/MencionEnNotaSeeder.php
@@ -29,26 +29,26 @@ class MencionEnNotaSeeder extends Seeder
             print_r($menciones->toSql() . "\n");
 
             foreach($menciones->get() AS $mencion){
-                print_r("Match:\t$mencion->clave\t$mencion->ubicacion\t$mencion->cod_nota\n");
-                MencionEnNota::create(array('cod_nota'=>$mencion->cod_nota, 'clave'=> $mencion->clave, 'ubicacion'=>$mencion->ubicacion));
+                print_r("Match:\t$mencion->mencion\t$mencion->ubicacion\t$mencion->cod_nota\n");
+                MencionEnNota::create(array('cod_nota'=>$mencion->cod_nota, 'mencion'=> $mencion->mencion, 'ubicacion'=>$mencion->ubicacion));
             }
         }
 
-        $pattern = '((d[^\d\w]{0,3}g[^\d\w]{0,3}n[^\d\w]{0,3}|[^\s>]*nmx)(([^\s])?[\w\d/]+)+(?=(,\s|\.\s|\s|<|\.?$$)))';
+        $pattern = '((d[^\d\w]{0,3}g[^\d\w]{0,3}n[^\d\w]{0,3}|[^\<s></s>]*nmx)(([^\s])?[\w\d/]+)+(?=(,\s|\.\s|\s|<|\.?$$)))';
 
         $menciones = DofNota::bodyContains($pattern);
 
         print_r($menciones->toSql() . "\n");
         foreach($menciones->get() AS $mencion){
-            if (strpos($mencion->titulo, $mencion->clave)==False){
+            if (strpos($mencion->titulo, $mencion->mencion)==False){
                 $mencion->ubicacion= 'Contenido';
             }else{
                 $mencion->ubicacion= 'Título';
             }
 
-            if (!in_array($mencion->clave, ['dgn1.html', 'dgnon','dgning', 'dgn.karla@economia.gob.mx', 'nmx.gob.mx/normasmx/index.nmx', 'nmx.gob.mx/normasmx/', 'nmx@prodigy.net.mx', "d;'>gnidad", 'nmx.carbonoforestal@semarnat.gob.mx', "d;'>gnar"])){
-                print_r("Match:\t$mencion->clave\t$mencion->ubicacion\t$mencion->cod_nota\n");
-                MencionEnNota::create(array('cod_nota'=>$mencion->cod_nota, 'clave'=> $mencion->clave, 'ubicacion'=>$mencion->ubicacion));
+            if (!in_array($mencion->mencion, ['dgn1.html', 'dgnon','dgning', 'dgn.karla@economia.gob.mx', 'nmx.gob.mx/normasmx/index.nmx', 'nmx.gob.mx/normasmx/', 'nmx@prodigy.net.mx', "d;'>gnidad", 'nmx.carbonoforestal@semarnat.gob.mx', "d;'>gnar"])){
+                print_r("Match:\t$mencion->mencion\t$mencion->ubicacion\t$mencion->cod_nota\n");
+                MencionEnNota::create(array('cod_nota'=>$mencion->cod_nota, 'mencion'=> $mencion->mencion, 'ubicacion'=>$mencion->ubicacion));
             }
         }
 
