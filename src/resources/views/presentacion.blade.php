844b9f4a15c6b2a8733e838d2ba616331402fc9b
Emir Herrera <emir.herrera@imco.org.mx>
2015-12-04 12:59:47 -0600

    Cache

--- src/resources/views/presentacion.blade.php
+++ src/resources/views/presentacion.blade.php
@@ -2,6 +2,7 @@
 	use IMCO\CatalogoNOMsApi\NormaVigente;
 	use IMCO\CatalogoNOMsApi\DofNota;
 	use IMCO\CatalogoNOMsApi\MencionEnNota;
+	//use Cache;
 ?>
 <html>
 
@@ -13,22 +14,22 @@ Se han descargado <?php echo App::make('IMCO\CatalogoNOMsApi\NormaVigente')->cou
 
 <p>Errores detectados:</p>
 <ul>
-	<li><a href="/catalogonoms/error/fecha-publicacion"><?php echo NormaVigente::conFechaPublicacionIncorrecta()->count();?> Normas vigentes</a> con una fecha de publicación en la que no existe publicación del Diario Oficial de la Federación</li>
-	<li><a href="/catalogonoms/error/no-localizadas"><?php echo NormaVigente::has('menciones', '<', 1)->count();?> Normas vigentes</a> que con el método actuál no han podido ser localizadas en publicaciones del DOF</li>
+	<li><a href="/catalogonoms/error/fecha-publicacion"><?php echo \Cache::remember('fechaErronea', 200, function() { return NormaVigente::conFechaPublicacionIncorrecta()->count();});?> Normas vigentes</a> con una fecha de publicación en la que no existe publicación del Diario Oficial de la Federación</li>
+	<li><a href="/catalogonoms/error/no-localizadas"><?php echo \Cache::remember('noLocalizada', 200, function() { return NormaVigente::has('menciones', '<', 1)->count();});?> Normas vigentes</a> que con el método actuál no han podido ser localizadas en publicaciones del DOF</li>
 </ul>
 
-<?php echo DofNota::count();?>
-
-<?php echo MencionEnNota::count();?>
-
-<?php echo MencionEnNota::select('mencion')->distinct('mencion')->count('mencion');?>
-
+Publicaciones del DOF <?php echo \Cache::remember('cuentaDOF', 200, function() { return DofNota::count();});?>
+<br/>
+Menciones de claves <?php echo \Cache::remember('cuentaMenciones', 200, function() { return MencionEnNota::count();});?>
+<br/>
+Menciones de claves únicas <?php echo \Cache::remember('cuentaMencionesUnicas', 200, function() { return MencionEnNota::select('mencion')->distinct('mencion')->count('mencion');});?>
+<br/>
 
 <p>Resultados:</p>
 <ul>
-	<li>De las <?php echo NormaVigente::count();?> normas vigentes se han localizado <a href="/catalogonoms/resultados/menciones"><?php echo NormaVigente::has('menciones', '>=', 1)->count();?> con menciones en el DOF</a></li>
-	<li><a href="/catalogonoms/resultados/proyectos"><?php echo NormaVigente::whereHas('menciones', function ($query){
-		$query->where('etiqueta', 'Proyecto');})->count(); ?> Normas con proyecto</a></li>
+	<li>De las <?php echo \Cache::remember('cuentaNormasVigentes', 200, function() { return NormaVigente::count();});?> normas vigentes se han localizado <a href="/catalogonoms/resultados/menciones"><?php echo \Cache::remember('cuentaNormasVigentesLocalizadas', 200, function() { return NormaVigente::has('menciones', '>=', 1)->count();});?> con menciones en el DOF</a></li>
+	<li><a href="/catalogonoms/resultados/proyectos"><?php echo \Cache::remember('cuentaNormasConProyecto', 200, function() { return NormaVigente::whereHas('menciones', function ($query){
+		$query->where('etiqueta', 'Proyecto');})->count();}); ?> Normas con proyecto</a></li>
 
 	
 </ul>
